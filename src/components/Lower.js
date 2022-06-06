163d54393f7a653802dce3b585e9a66dc321c601
unknown <goodcoder2000@gmail.com>
2022-06-06 11:44:35 +0630

    second test

--- src/components/Lower.js
+++ src/components/Lower.js
@@ -4,10 +4,16 @@ import { Right } from "./Right";
 import Grid from '@mui/material/Grid';
 import { Chat } from "./Chat";
 import Hidden from "@mui/material/Hidden";
+import { Skills } from "../page/Skills";
+import { Projects} from "../page/Projects";
+import { Contact } from "../page/Contact";
+import { Portfolio } from "../page/Portfolio";
+import { Footer } from "./Footer";
 
 export const Lower = () =>{
     return(
-        <div className="lowerContainer">
+        <div>
+            <div className="lowerContainer">
             <Grid container spaceing={2}>
                 <Grid item lg={7} md={12}>
                     <Left/>
@@ -16,10 +22,17 @@ export const Lower = () =>{
                     <Right/>
                 </Grid>
             </Grid>
+
+            <Contact/>
+            <Portfolio/>
+            <Skills/>
+            <Projects/>
         
             <Hidden only={["lg","md"]}>
                 <Chat/>
             </Hidden>
+        </div>
+        <Footer/>
         </div>
     );
 }
